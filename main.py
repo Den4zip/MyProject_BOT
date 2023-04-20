00a7d0cd812726b39470dfca7c330512b08dea5a
Влад <piskadodra5@gmail.com>
2023-04-20 18:02:30 +0300

    Мелкие изменения

--- main.py
+++ main.py
@@ -9,20 +9,29 @@ MAX_COUNTER: int = 30  # время работы
 offset: int = -2
 counter: int = 0
 chat_id: int
-
+text: str
+update_id: int
 if __name__ == '__main__':
     while counter < MAX_COUNTER:
+        print('attempt =', counter)# Чтобы видеть в консоли, что код живет
 
-        print('attempt =', counter)  # Чтобы видеть в консоли, что код живет
-
-        updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()  # апдейты
-
-        if updates['result']:
-            for result in updates['result']:
+        #  updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?').json()
+        updates1 = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset{offset - 1}').json()
+        if updates1['result']:
+            for result in updates1['result']:
                 offset = result['update_id']
                 chat_id = result['message']['from']['id']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')  # отправка сообщения
+                # text = result['message']['text']
+                # update_id = result['update_id']
+            #    requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')  # отправка сообщения
 
         time.sleep(1)
         counter += 1
-        pprint(updates)
+        # pprint(updates)
+        # print()
+        pprint(updates1)
+        # print(text)
+        # print(update_id)
+        #  pprint(updates2)
+        #  pprint(updates3)
+
